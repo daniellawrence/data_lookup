443758e9ffcbba9405e8bd1277a4cfa90f93b527
Daniel Lawrence <dannyla@linux.com>
2016-10-30 21:04:20 -0700

    ideas

---
+++ main.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+import copy
+import os
+import re
+import pprint
+import json
+import hashlib
+
+# import pymongo
+import yaml
+
+# Used to rank the datasources that can overwrite the others
+# <schema>://<database>/<path>
+ladder = [
+    'yaml://artisan_yaml/{node_name}.yaml',
+    'yaml://artisan_yaml/{role_name}.yaml',
+    'yaml://artisan_yaml/{fabric}.yaml',
+    'yaml://artisan_yaml/{environment}.yaml',
+    'yaml://artisan_yaml/{osfamily}.yaml',
+    'yaml://artisan_yaml/{os}.yaml',
+    'yaml://artisan_yaml/{env}.yaml',
+    'yaml://artisan_yaml/base.yaml',
+    #
+    'mongo://fact_db/{node_name}',
+    # 'https://remote-webservice/api/v0/{node_name}',
+    #
+    'dict://node/{node_name}',
+]
+
+# used to mock out "node" facts
+node = {
+    'xps13z': {
+        'node_name': 'xps13z',
+        'role_name': 'laptop',
+        'os': 'linux',
+    }
+}
+
+
+def dict2sha1(data):
+    if isinstnace(data, dict):
+        data = json.loads(data)
+
+    m = hashlib.sha1()
+    m.update(str(data))
+    data_sha1 = m.hexdigest()
+    return data_sha1
+
+
+def ladder_parser(uri):
+    """ Given an URI fetch the schema, database and lookup(key) """
+    uri_re = re.compile(r"(?P<schema>.*)://(?P<database>.+)/(?P<lookup>.*)")
+    uri_match = uri_re.match(uri)
+    (schema, database, lookup) = uri_match.groups()
+
+    if schema not in lookup_schemas:
+        error = "schema '{0}' invalid, must be one of: {1}"
+        allowed_schemas = ",".join(lookup_schemas)
+        raise Exception(error.format(schema, allowed_schemas))
+
+    return schema, database, lookup
+
+
+def loop_merge(current, new, take_first_result=False, level=0):
+
+    for new_key, new_data in new.items():
+        if new_key not in current:
+            current[new_key] = copy.copy(new_data)
+            continue
+
+        current_data = current[new_key]
+
+        if isinstance(new_data, list):
+            if take_first_result and new_key in current:
+                continue
+
+            if not isinstance(current_data, list):
+                current_data = list(current_data)
+
+            current[new_key] += new_data
+            continue
+
+        if isinstance(new_data, dict):
+
+            if isinstance(current_data, dict):
+                current[new_key] = loop_merge(current_data, new_data, level+1)
+                continue
+
+        current[new_key] = new_data
+
+    return current
+
+
+def lookup_data_yaml(database, lookup_value):
+    file_path = os.path.join(database, lookup_value)
+    if not os.path.exists(file_path):
+        return None
+    return yaml.load(open(file_path).read())
+
+
+def lookup_data_json(database, lookup_value):
+    file_path = os.path.join(database, lookup_value)
+    if not os.path.exists(file_path):
+        return None
+    return json.load(open(file_path).read())
+
+
+def lookup_data_dict(database, lookup_value):
+    return globals().get(database).get(lookup_value)
+
+
+def lookup_data_mongo(database, lookup_value):
+    # client = pymongo.MongoClient()
+    # db = client[database]
+    # return db[lookup_value]
+    pass
+
+lookup_schemas = {
+    'yaml': lookup_data_yaml,
+    'json': lookup_data_json,
+    'dict': lookup_data_dict,
+    'mongo': lookup_data_mongo,
+}
+
+
+def resolver(node_name):
+    catalog_first = {}
+    catalog_merge = {}
+
+    for ladder_item in ladder:
+        schema, database, lookup_path = ladder_parser(ladder_item)
+
+        try:
+            lookup_value = lookup_path.format(**node.get(node_name, {}))
+        except KeyError:
+            continue
+
+        fetched_value = None
+
+        if schema not in lookup_schemas:
+            continue
+
+        lookup_function = lookup_schemas.get(schema)
+        fetched_value = lookup_function(database, lookup_value)
+
+        if not fetched_value:
+            continue
+
+        loop_merge(catalog_first, fetched_value, take_first_result=True)
+        loop_merge(catalog_merge, fetched_value, take_first_result=False)
+
+    return catalog_first, catalog_merge
+
+
+if __name__ == '__main__':
+    f, m = resolver('xps13z')
+    print "-" * 80
+    pprint.pprint(f)
+    pprint.pprint(m)
---
+++ web.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+from main import resolver, node, ladder, lookup_schemas
+import json
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/api/')
+def api_GET():
+    rules = []
+    for rule in app.url_map.iter_rules():
+        rules.append(str(rule))
+    return jsonify({'api': rules})
+
+
+@app.route('/api/ladder')
+def api_GET_fetch_ladder():
+    return jsonify({'ladder': ladder})
+
+
+@app.route('/api/direct/schema/<schema>/database/<database_name>/path/<path:lookup_value>/')
+def api_GET_fetch_database(schema, database_name, lookup_value):
+    lookup_function = lookup_schemas.get(schema)
+    fetched_value = lookup_function(database_name, lookup_value)
+    return jsonify(fetched_value)
+
+
+@app.route('/api/node/')
+def api_GET_nodes():
+    return jsonify({'nodes': node.keys()})
+
+
+@app.route('/api/node/<node_name>/')
+def api_GET_node(node_name):
+    return jsonify(node.get(node_name))
+
+
+@app.route('/api/node/<node_name>/', methods=['POST'])
+def api_POST_node(node_name):
+    new_node_data_raw = request.form.copy()
+    new_node_data = json.load(new_node_data_raw)
+    node[node_name].update(new_node_data)
+    return node.get(node_name, {})
+
+
+@app.route('/api/facts/<node_name>/merged/')
+def api_GET_merged_facts_node(node_name):
+    first, merged = resolver(node_name)
+    return jsonify(merged)
+
+
+@app.route('/api/facts/<node_name>/first/')
+def api_GET_first_facts_node(node_name):
+    first, merged = resolver(node_name)
+    return jsonify(first)
+
+
+@app.route('/api/facts/<node_name>/FACTS/')
+def api_GET_keys_fact_node(node_name):
+    first, merged = resolver(node_name)
+    return jsonify({'facts': first.keys()})
+
+
+@app.route('/api/facts/<node_name>/first/<fact_name>/')
+def api_GET_first_facts_node_single_fact(node_name, fact_name):
+    first, merged = resolver(node_name)
+    return jsonify({fact_name: first.get(fact_name, {})})
+
+
+@app.route('/api/facts/<node_name>/merged/<fact_name>/')
+def api_GET_merged_facts_node_single_fact(node_name, fact_name):
+    first, merged = resolver(node_name)
+    return jsonify({fact_name: merged.get(fact_name, {})})
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
